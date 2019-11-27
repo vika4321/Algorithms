99ba60e5a27f1264ce740ac24a7e3acbeffa8e70
Victoria Stampfli <vika4321@yahoo.com>
2019-11-27 09:41:42 -0500

    added spiral algorithm

---
+++ Algorithms.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfHello
+{
+	class Algorithms
+	{
+
+		public List<int> spiralOrder(List<List<int>> A)
+		{
+			List<int> result = new List<int> { };
+
+			int rows = 0;
+			int cols = 0;
+			foreach (var sublist in A)
+			{
+				cols = sublist.Count();
+				rows++;
+			}
+			int top = 0;
+			int bottom = rows-1;
+			int left = 0;
+			int right = cols-1;
+
+			int dir = 0;
+			while (top <= bottom && left <= right)
+			{
+				if (dir == 0)
+				{
+					for (int i = left; i <= right; i++)
+					{
+						result.Add(A[top][i]);
+					} //for
+					top++;
+					dir = 1;
+				}//if
+				else if (dir == 1)
+				{
+
+					for (int i = top; i <= bottom; i++)
+					{
+						result.Add(A[i][right]);
+					}
+					right--;
+					dir = 2;
+				}//else if
+				else if (dir == 2)
+				{
+					for (int i = right; i >= left; i--)
+					{
+						result.Add(A[bottom][i]);
+
+					}
+					bottom--;
+					dir = 3;
+				}
+				else if (dir == 3)
+				{
+					for (int i = bottom; i >= top; i--)
+					{
+						result.Add(A[i][left]);
+					}
+					left++;
+					dir = 0;
+				}
+
+			} //while
+			return result;
+		}
+	}
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfHello
+{
+	/// <summary>
+	/// Interaction logic for MainWindow.xaml
+	/// </summary>
+	public partial class MainWindow : Window
+	{
+		public MainWindow()
+		{
+			InitializeComponent();
+			Algorithms obj = new Algorithms();
+			List<List<int>> SetArray = new List<List<int>>{
+				new List<int> { 5, 7, 6},
+				new List<int> { 1, 5, 6}
+			};
+			List<int> r = obj.spiralOrder(SetArray);
+		}
+
+		private void ok_Click(object sender, RoutedEventArgs e)
+		{
+			MessageBox.Show("Hello " + userName.Text);
+		}
+	}
+}
